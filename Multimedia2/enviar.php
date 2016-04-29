d5e66b4fcdd43d2904328e0bbd8afa80802a701b
Victor Sanchez <vicsmr94@gmail.com>
2016-04-29 15:03:01 +0200

    Añadido enviado y cambios

--- Multimedia2/enviar.php
+++ Multimedia2/enviar.php
@@ -1,20 +1,19 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
+    $to = $_POST['nombre']; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['nombre'];
     $last_name = $_POST['apellido'];
     $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
+    $subject2 = "Postal de Valencia";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['mensaje'];
     $message2 = "Mensaje de " . $first_name . "\n\n" . $_POST['mensaje'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
+    mail($from,$subject2,$message2,$headers2); 
+    
+    header('Location: Enviado.html');
     }
 ?>
